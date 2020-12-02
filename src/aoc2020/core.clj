718601631b9071dc2ee261b17adf85a1ff993dae
Schmitz, Arne <arne.schmitz@nuance.com>
2020-12-02 16:57:03 +0100

    day 2 part one

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -45,11 +45,57 @@
   (calc-product 3 input)
   )
 
+;; day 2
+(defn parse-password-line
+  [str]
+  (let
+      [p (first (re-seq #"^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" str))]
+    (vector (Integer. (nth p 1))
+            (Integer. (nth p 2))
+            (first (nth p 3))
+            (nth p 4)
+            )
+    )
+  )
+
+(defn read-password-input
+  [input-file]
+  (let [input (slurp input-file)]
+    (map parse-password-line (clojure.string/split-lines input))
+    )
+  )
+
+(defn test-valid-password
+  "A valid password P has minimum X and maximum Y occurences of char C"
+  [x y c p]
+  (let [countc (count (filter #(= c %) p))]
+    (and (>= y countc) (<= x countc))
+    )
+)
+
+(defn is-valid-password?
+  "Returns true if the password is valid"
+  [input]
+  (apply test-valid-password input)
+  )
+
+(defn count-valid-passwords
+  "Counts the number of valid passwords in the input"
+  [input]
+  (->> input
+       (filter is-valid-password?)
+       (count)
+       )
+  )
+
 (defn -main
   "Advent of Code 2020."
   [& args]
   (let [input (read-input "resources/input_1.txt")]
     (println "1.1 Given X + Y = 2020 we have X * Y =" (calc-two-product input) )
     (println "1.2 Given X + Y + Z = 2020 we have X * Y * Z =" (calc-three-product input) )
+    )
+  (let [input (read-password-input "resources/input_2.txt")]
+    (println "2.1 Number of valid passwords: " (count-valid-passwords input))
     )
   )
