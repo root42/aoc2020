13da80483465856337ce519e9a57b80c9aa6f838
Schmitz, Arne <arne.schmitz@nuance.com>
2020-12-14 11:37:48 +0100

    day 13.2

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -1,6 +1,7 @@
 (ns aoc2020.core
   (:gen-class)
-  (:require [clojure.set :refer [difference union intersection]]))
+  (:require [clojure.set :refer [difference union intersection]])
+  (:require [clojure.math.numeric-tower :as math]))
 
 (use 'clojure.math.combinatorics)
 (use 'clojure.set)
@@ -811,6 +812,64 @@
     )
   )
 
+(defn extended-gcd
+  "The extended Euclidean algorithm
+  Returns a list containing the GCD and the Bézout coefficients
+  corresponding to the inputs. "
+  [a b]
+  (cond (zero? a) [(math/abs b) 0 1]
+        (zero? b) [(math/abs a) 1 0]
+        :else (loop [s 0
+                     s0 1
+                     t 1
+                     t0 0
+                     r (math/abs b)
+                     r0 (math/abs a)]
+                (if (zero? r)
+                  [r0 s0 t0]
+                  (let [q (quot r0 r)]
+                    (recur (- s0 (* q s)) s
+                           (- t0 (* q t)) t
+                           (- r0 (* q r)) r))))))
+ 
+(defn chinese-remainder
+  " Main routine to return the chinese remainder "
+  [n a]
+  (let [prod (apply * n)
+        reducer (fn [sum [n_i a_i]]
+                  (let [p (quot prod n_i)           ; p = prod / n_i
+                        egcd (extended-gcd p n_i)   ; Extended gcd
+                        inv_p (second egcd)]        ; Second item is the inverse
+                    (+ sum (* a_i inv_p p))))
+        sum-prod (reduce reducer 0 (map vector n a))] ; Replaces the Python for loop to sum
+                                                      ; (map vector n a) is same as
+        ;                                             ; Python's version Zip (n, a)
+    (mod sum-prod prod)))                             ; Result line
+
+(defn read-crt-input
+  [input]
+  (let [lines (slurp input)
+        values (->> lines
+                    clojure.string/split-lines
+                    second
+                    (re-seq #"[^,]+"))
+        n (->> values
+               (filter #(not= "x" %))
+               (map #(Integer/parseInt %))
+               )
+        a (->> values
+               (map-indexed
+                (fn [idx itm]
+                  (if (not= "x" itm)
+                    (let [x (Integer/parseInt itm)]
+                      (mod (- x (mod idx x)) x)))))
+               (filter #(not (nil? %)))
+           )
+        ]
+    [n a]
+    )
+  )
+
 (defn -main
   "Advent of Code 2020."
   [& args]
@@ -865,5 +924,8 @@
     )
   (let [input (read-bus-input "resources/input_13.txt")]
     (println "13.1 Earliest bus times waiting time in minutes: " (earliest-bus input))
+    )
+  (let [[n a] (read-crt-input "resources/input_13.txt")]
+    (println "13.2 Earliest time when all buses leave staggered: " (chinese-remainder n a))
     )
   )
